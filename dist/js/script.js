de1c1a45436a91b557f7d2a46e165f1c926d2f35
Max <maximpalaguta@gmail.com>
2021-08-14 01:28:46 +0300

    Added new section <<slider>>

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -14,4 +14,43 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('menu_active');
         })
     })
+
+
+    $(".slider__slick-slider").slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        arrows: false,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1590,
+                settings: {
+                    slidesToShow: 4,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1257,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 958,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 })
